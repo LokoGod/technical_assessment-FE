1b7cbe40bde31be7d9a67841d6b18ad29a2c1632
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-01-31 19:39:05 +0530

    predefining CSS variables

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,6 +2,12 @@ export default function Navbar() {
     return (
         <div>
             <h1>Modern Walk</h1>
+            <h2>Modern Walk</h2>
+            <h3>Modern Walk</h3>
+            <h4>Modern Walk</h4>
+            <h5>Modern Walk</h5>
+            
+            <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
         </div>
     )
 };
