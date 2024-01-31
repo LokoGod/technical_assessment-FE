17840e159b861e10cfe2ada11d4855bca058db0b
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-02-01 01:13:51 +0530

    fethcingDataover

--- client/src/components/sections/Footer.tsx
+++ client/src/components/sections/Footer.tsx
@@ -2,10 +2,7 @@ export default function Footer() {
     return (
       <>
         <footer>
-        <hr className="h-1.5 mb-8 bg-gray-300 border-0"></hr>
-          <div className="my-3 text-center">
-            <a href="/" className="font-bold text-5xl">Modern Walk</a>
-          </div>
+        <div className=" pb-10"></div>
 
         </footer>
       </>
