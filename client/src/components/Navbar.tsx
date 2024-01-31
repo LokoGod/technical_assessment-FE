6bc0160a889945bba51ddda888b492801f703708
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-01-31 19:56:36 +0530

    navbar feature completion

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,12 @@
 export default function Navbar() {
-    return (
-        <div>
-            <h1>Modern Walk</h1>
-            <h2>Modern Walk</h2>
-            <h3>Modern Walk</h3>
-            <h4>Modern Walk</h4>
-            <h5>Modern Walk</h5>
-            
-            <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+  return (
+    <>
+      <header>
+        <div className="my-3 text-center">
+          <a href="/" className="font-bold text-5xl">Modern Walk</a>
         </div>
-    )
-};
+        <hr className="h-1.5 mb-8 bg-gray-300 border-0"></hr>
+      </header>
+    </>
+  );
+}
