7d5f1b00dfddbf251568344f3d824b9bc00f0e77
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-02-01 10:44:24 +0530

    ui

--- client/src/components/sections/Navbar.tsx
+++ client/src/components/sections/Navbar.tsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <>
       <header>
-        <div className="my-3 text-center">
+        <div className="mb-3 pt-3 text-center">
           <a href="/" className="font-bold text-5xl">Modern Walk</a>
         </div>
         <hr className="h-1.5 mb-8 bg-gray-300 border-0"></hr>
