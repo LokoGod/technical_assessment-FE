fc8cdca04af2ddc1e22345fd9e634931dd7049ef
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-02-01 10:43:03 +0530

    ui

--- client/src/components/reusables/Cards.tsx
+++ client/src/components/reusables/Cards.tsx
@@ -1,19 +1,14 @@
-import Image from "next/image";
-import MensJacket from "../../../public/Mens Jacket.png";
-
-import { Button } from "@mui/material";
-
 export default function Cards() {
   return (
     <>
     <div className="flex justify-between">
-    <a href="/mensClothing" className=" w-[650px] h-48 rounded-3xl overflow-hidden shadow-xl bg-emerald-400">
+    <a href="/mens-clothing" className=" w-[650px] h-48 rounded-3xl overflow-hidden shadow-xl bg-emerald-400">
         <div className="py-4 text-center">
           <div  className="font-bold text-3xl mb-2 text-white">Men's Clothing</div>
         </div>
       </a>
 
-      <a href="/womensClothing" className=" w-[650px] rounded-3xl overflow-hidden shadow-xl bg-rose-400">
+      <a href="/womens-clothing" className=" w-[650px] rounded-3xl overflow-hidden shadow-xl bg-rose-400">
         <div className="py-4 text-center">
           <div  className="font-bold text-3xl mb-2 text-white">Women's Clothing</div>
         </div>
--- client/src/components/sections/Categories.tsx
+++ client/src/components/sections/Categories.tsx
@@ -1,12 +1,31 @@
-import Cards from "../reusables/Cards";
-
 export default function Categories() {
-    return (
-        <>
-        <h2 className="mb-5">Categories</h2>
+  return (
+    <>
+      <h2 className="mb-5">Categories</h2>
 
-        <Cards />
+      <div className="flex justify-between">
+        <a
+          href="/mens-clothing"
+          className=" w-[650px] h-48 rounded-3xl overflow-hidden shadow-xl bg-emerald-400"
+        >
+          <div className="py-4 text-center">
+            <div className="font-bold text-3xl mb-2 text-white">
+              Men's Clothing
+            </div>
+          </div>
+        </a>
 
-        </>
-    )
-};
+        <a
+          href="/womens-clothing"
+          className=" w-[650px] rounded-3xl overflow-hidden shadow-xl bg-rose-400"
+        >
+          <div className="py-4 text-center">
+            <div className="font-bold text-3xl mb-2 text-white">
+              Women's Clothing
+            </div>
+          </div>
+        </a>
+      </div>
+    </>
+  );
+}
